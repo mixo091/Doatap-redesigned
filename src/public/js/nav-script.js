26adb5389b71d6f6310ee672df6a1b46c92161b5
mixo091 <mpla0268@gmail.com>
2022-01-16 17:01:41 +0200

    styling part-1

--- src/public/js/nav-script.js
+++ src/public/js/nav-script.js
@@ -12,3 +12,20 @@ closeMenu.addEventListener('click' , () => {
     Menu.classList.toggle('show')
     toggleMenu.classList.toggle('dissapear')
 })
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("mpla");
+}
+      
+// Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show1')) {
+        myDropdown.classList.remove('show1');
+          }
+        }
+      }
